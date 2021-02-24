c59e3f2a9a3ad76711b0d1a52467019db3fff4f5
sandro-gassner <77922210+sandro-gassner@users.noreply.github.com>
2021-02-24 18:06:14 +0100

    Add files via upload

---
+++ R_CODE_SIC_NAICS_MAPPING.R
@@ -0,0 +1,27 @@
+# Notwendige Packages laden
+library(readxl)
+library(writexl)
+library(dplyr)
+library(tidyr)
+
+# NAICS SIC Crosswalk laden
+crosswalk <- read_xlsx("NAICS_SIC_Crosswalk_2.xlsx")
+
+# Ratios laden
+ratios <- read_xlsx("SIC_ratios.xlsx")
+
+# Ratios zum Datensatz hinzufügen
+library(plyr)
+crosswalk <- join(crosswalk, ratios, by="SIC_L1")
+detach("package:plyr", unload=TRUE)
+
+# Data-Cleaning
+crosswalk <- crosswalk[complete.cases(crosswalk), ]
+
+# Durchschnittliche Ratios pro NAICS Gruppe
+mean_ratios <- crosswalk %>% group_by(NAICS_L1) %>% 
+  summarize(mean_de_ratio = mean(de_ratio),
+            mean_q_ratio = mean(q_ratio))
+
+# Excel-Datei erstellen
+write_xlsx(mean_ratios, "mean_ratios_naics.xlsx")
